f789562ca1d9cc986db2e50857a84e55982699ef
Dan Egnor <egnor@ofb.net>
2026-05-31 23:39:47 -0700

    more elaborate chunker

--- ok_serial/terminal/escape_code_chunker.py
+++ ok_serial/terminal/escape_code_chunker.py
@@ -12,16 +12,25 @@ class EscapeCodeChunker:
         b"[\xc0-\xdf][\x80-\xbf]|"
         b"[\xe0-\xef][\x80-\xbf]{2}|"
         b"[\xf0-\xf7][\x80-\xbf]{3}"
-        b")*)|"
-        # group 2: *incomplete* UTF-8 code point at end of data
+        b")+)|"
+        # group 2: *partial* UTF-8 code point at end of data
         b"([\xc0-\xf7][\x80-\xbf]*$)|"
-        # group 3: one complete VTxxx CSI control sequence
-        # https://vt100.net/emu/dec_ansi_parser (not incl. DCS, OSC, etc)
-        b"((?:\x9b|\x1b\\[)[\x20-\x3f]*[\x40-\x7e])|"
-        # group 4: *incomplete* VTxxx CSI control sequence at end of data
-        b"(\1b|\x1b\\[\x20-\x3f]*$|\x9b[\x20-\x3f]*$)|"
+        # group 3: one complete VTxxx control sequence
+        # https://vt100.net/emu/dec_ansi_parser
+        b"("
+        b"(?:\x1b\\[|\x9b)[\x20-\x3f]*[\x40-\x7e]|"  # CSI
+        b"(?:\x1b[\x50\x58\\]-\x5f]|[\x98\x9d-\x9f])"  # DCS/SOS/OSC/PM/APC
+        b"[\x20-\x7f]*(?:\x07|\x9c|\x1b\\\\)|"  # DCS/SOS/OSC/PM/APC con't
+        b"\x1b[\x30-\x4f\x51-\x57\x59\x5a\x60-\x7e]"  # ESC-controls
+        b")|"
+        # group 4: *partial* VTxxx control sequence at end of data
+        b"("
+        b"\x1b$|"  # ESC by itself
+        b"(?:\x1b\\[|\x9b)[\x20-\x3f]*$|"  # CSI
+        b"(?:\x1b[\x50\x58\\]-\x5f]|[\x98\x9d-\x9f])[\x20-\x7f]*\x1b?$"  # etc
+        b")|"
         # group 5: any other byte (control code, invalid, etc)
-        b"[\x00-\xff]"
+        b"([\x00-\xff])"
     )
 
     TIMEOUT = 0.1  # seconds to pause before giving up on partial data
@@ -55,8 +64,9 @@ class EscapeCodeChunker:
                 assert len(other) == 1, other
                 pos += 1
             else:
-                assert self._partial in (char_part, esc_part)
+                self._partial = self._partial[pos:]
                 self._partial_timeout = data_time + self.TIMEOUT
+                assert self._partial in (char_part, esc_part), match.groups()
                 return
 
         self._partial.clear()
@@ -64,8 +74,8 @@ class EscapeCodeChunker:
 
     def get_chunks(self) -> list[str | bytes]:
         """Gets accumulated data broken into chunks:
-        - str for characters to be output
-        - bytes for control sequences to be interpreted
+        - str for well formed UTF-8 characters
+        - bytes for control characters or escape sequences to be interpreted
         """
         out, self._chunks = self._chunks, []
         return out
