0a87f594238cca70706af8b3060306cfcbc36f0e
Dan Egnor <egnor@ofb.net>
2026-07-21 06:49:02 -0700

    rename

--- ok_serial/_terminal_decorator.py
+++ ok_serial/_terminal_decorator.py
@@ -1,8 +1,13 @@
+import re
 from typing import Literal
 
 from ok_serial._terminal_mode_tracker import TerminalModeTracker
 
 CURSOR_QUERY_TIMEOUT = 1.0  # seconds
+
+CURSOR_QUERY_RX = re.compile(b"(?:\x1b\\[|\x9b)6n")
+
+CURSOR_REPLY_RX = re.compile(b"(?:\x1b\\[|\x9b)(\\d+);(\\d+)R")
 
 
 class TerminalDecorator:
@@ -43,27 +48,47 @@ class TerminalDecorator:
         self.add_input: list[bytes | str] = []
         self.out_input: list[bytes | str] = []
 
+        # terminal mode tracking: the mode set by base content, the mode
+        # to use for decorations, and what the terminal is actually doing
         self._base_mode = TerminalModeTracker()
         self._decoration_mode = TerminalModeTracker()
         self._active_mode = self._base_mode
 
+        # cursor tracking: the base cursor column, and cursor excursion status
+        # (between cols, the cursor *row* remains aligned with the base cursor)
         self._base_col: int | Literal["unknown", "querying"] = "unknown"
         self._cursor_pos: Literal["base", "roam"] = "base"
-        self._query_deadlines: list[float] = []
+        self._query_deadlines: list[float] = []  # cursor query timeouts
 
+        # currently displayed right/below decorations for comparison
+        # (above decorations are inserted and left in place forever)
         self._now_below: list[list[bytes | str]] = []
         self._now_right: list[bytes | str] = []
 
-        self._decoration_mode.add_chunk(b"\x1b[?7l")  # no wrap
+        # disable linewrap for decorations to avoid cursor tracking problems
+        self._decoration_mode.add_chunk(b"\x1b[?7l")
 
     def update(self, time: float) -> None:
         """Processes input properties and updates output properties.
         - time: clock time in seconds (with any consistent epoch)
         """
 
-        # TODO: process input
+        # match cursor position replies to queries, including ours
+        # note, this assumes no additional queries issued in "querying" state
+        for chunk in self.add_input:
+            if isinstance(chunk, bytes) and (m := CURSOR_REPLY_RX.match(chunk)):
+                del self._query_deadlines[:1]
+                if self._base_col == "querying" and not self._query_deadlines:
+                    self._base_col = int(m.group(2))
+        self.add_input.clear()
 
-        # trim decorations right/below of cursor if:
+        # time out expired queries if the response failed somehow
+        while self._query_deadlines and time > self._query_deadlines[0]:
+            del self._query_deadlines[0]
+        if self._base_col == "querying" and not self._query_deadlines:
+            self._base_col = "unknown"
+
+        # strategize - trim decorations right/below of cursor if:
         # - base content is pending *and* reachable / ready to update
         # - OR right/below decoration content has changed
         if self.add_base and (
@@ -79,14 +104,14 @@ class TerminalDecorator:
                 if self.set_below[keep_below] != self._now_below[keep_below]:
                     break
 
-        # delete right if needed and possible
+        # clear right of cursor if requested and possible
         if clear_right and self._can_return_to_base():
             self._switch_mode(self._decoration_mode)
             self._return_to_base()
             self._emit(b"\x1b[K")
             self._now_right.clear()
 
-        # delete rows below if needed
+        # delete below decoration rows if requested
         if del_below := len(self._now_below) - keep_below:
             assert del_below > 0, (self._now_below, keep_below)
             self._switch_mode(self._decoration_mode)
@@ -106,6 +131,10 @@ class TerminalDecorator:
             self._return_to_base()
             self._switch_mode(self._base_mode)
             self._emit(*self.add_base)
+            for chunk in self.add_base:
+                # track queries from base content
+                if isinstance(chunk, bytes) and CURSOR_QUERY_RX.match(chunk):
+                    self._query_deadlines.append(time + CURSOR_QUERY_TIMEOUT)
             self.add_base.clear()
             self._base_col = "unknown"  # can't predict ending point
 
@@ -129,8 +158,8 @@ class TerminalDecorator:
                 b"\x1b[%dL" % len(self.add_above),  # insert rows
             )
             for above_line in self.add_above:
-                self._switch_mode(self._decoration_mode.copy())
-                self._emit(b"\r", *above_line, b"\n")
+                self._switch_mode(self._decoration_mode.copy())  # start fresh
+                self._emit(b"\r", *above_line, b"\n")  # ends at base row
             self.add_above.clear()
 
         # insert lines below if requested
@@ -144,7 +173,7 @@ class TerminalDecorator:
             for below_line in self.set_below[skip_lines:]:
                 self._switch_mode(self._decoration_mode.copy())
                 self._emit(b"\r", b"\n", *below_line)
-            self._emit(b"\x1b[%dA" % len(self.set_below))
+            self._emit(b"\x1b[%dA" % len(self.set_below))  # ends at base row
 
         # for aesthetics, leave the cursor at base pos, if possible
         if self._can_return_to_base():
